11b23e0542cf7f934b79974062c649eb3190897a
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2022-12-12 14:12:25 +0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author '𝐒 𝐘 𝐍 𝐎#1760'
+description 'SY_Notify'
+version '1.0'
+
+lua54 'yes'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
+
+export 'Alert'
